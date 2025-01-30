564be47ff6527eace9ce89a02f8a0e9020d26769
Albert Ivanov <albertivanov187@gmail.com>
2025-01-30 02:34:57 +0200

    100/100

---
+++ 02. StackSum/Program.cs
@@ -0,0 +1,38 @@
+﻿
+int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+Stack<int> stack = new Stack<int>(numbers);
+
+string[] commands = Console.ReadLine().ToLower().Split();
+
+while (commands[0] != "end")
+{
+    if (commands[0] == "add")
+    {
+        int firstNumber = int.Parse(commands[1]);
+        int secondNumber = int.Parse(commands[2]);
+
+        stack.Push(firstNumber);
+        stack.Push(secondNumber);
+    }
+    if (commands[0] == "remove")
+    {
+        int count = int.Parse(commands[1]);
+
+        for (int i = 0; i < count; i++)
+        {
+            if (stack.Count >= count)
+            {
+                stack.Pop();
+            }
+            else
+            {
+                break;
+            }
+        }
+    }
+
+    commands = Console.ReadLine().ToLower().Split();
+}
+
+Console.WriteLine($"Sum: {stack.Sum()}");
