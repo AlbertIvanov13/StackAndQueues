8e4a31296da75e0568fc7b84768a32cbf3e75f63
Albert Ivanov <albertivanov187@gmail.com>
2025-01-30 12:37:56 +0200

    100/100

---
+++ 03. SimpleCalculator/Program.cs
@@ -0,0 +1,39 @@
+﻿
+using System.Data;
+
+string[] numbers = Console.ReadLine().Split().ToArray();
+
+Array.Reverse(numbers);
+
+Stack<string> stack = new Stack<string>();
+
+int sum = 0;
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    stack.Push(numbers[i]);
+}
+
+int firstNumber = 0;
+int secondNumber = 0;
+bool first = true;
+while (stack.Count > 0)
+{
+    string character = stack.Pop();
+    if (character == "+")
+    {
+        int nextNumber = int.Parse(stack.Pop());
+        sum += nextNumber;
+    }
+    else if (character == "-")
+    {
+        int nextNumber = int.Parse(stack.Pop());
+        sum -= nextNumber;
+    }
+    else
+    {
+        sum += firstNumber = int.Parse(character.ToString());
+    }
+}
+
+Console.WriteLine(sum);
