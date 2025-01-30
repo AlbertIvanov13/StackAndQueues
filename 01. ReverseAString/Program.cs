7490f7cadd5d3b4c1cee139c8cbbf25e735ca546
Albert Ivanov <albertivanov187@gmail.com>
2025-01-30 02:02:09 +0200

    Add project files.

---
+++ 01. ReverseAString/Program.cs
@@ -0,0 +1,11 @@
+﻿
+string input = Console.ReadLine();
+
+Stack<char> stack = new Stack<char>();
+
+for (int i = 0; i < input.Length; i++)
+{
+    stack.Push(input[i]);
+}
+
+Console.WriteLine(string.Join("", stack));
