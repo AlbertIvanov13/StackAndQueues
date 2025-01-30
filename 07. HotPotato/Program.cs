ce7e37442b219d570223538d9c4ad850b8c8a515
Albert Ivanov <albertivanov187@gmail.com>
2025-01-30 15:59:47 +0200

    100/100

---
+++ 07. HotPotato/Program.cs
@@ -0,0 +1,23 @@
+﻿
+string[] kids = Console.ReadLine().Split();
+int throws = int.Parse(Console.ReadLine());
+
+Queue<string> queue = new Queue<string>(kids);
+
+int count = 0;
+while (queue.Count > 1)
+{
+    string kid = queue.Dequeue();
+    count++;
+    if (count < throws)
+    {
+        queue.Enqueue(kid);
+    }
+    else if (count == throws)
+    {
+        Console.WriteLine($"Removed {kid}");
+        count = 0;
+    }
+}
+
+Console.WriteLine($"Last is {string.Join("", queue)}");
