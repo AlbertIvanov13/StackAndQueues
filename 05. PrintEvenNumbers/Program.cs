0cf24a63cf329945fd52f880202a452ff3dd477d
Albert Ivanov <albertivanov187@gmail.com>
2025-01-30 15:20:35 +0200

    100/100

---
+++ 05. PrintEvenNumbers/Program.cs
@@ -0,0 +1,16 @@
+﻿
+int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+Queue<int> queue = new Queue<int>(numbers);
+Queue<int> newQueue = new Queue<int>();
+
+while (queue.Count > 0)
+{
+    int number = queue.Dequeue();
+    if (number % 2 == 0)
+    {
+        newQueue.Enqueue(number);
+    }
+}
+
+Console.WriteLine(string.Join(", ", newQueue));
