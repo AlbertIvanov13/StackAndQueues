8cb502e7e2742772f68b2b387c41b0725a5c5f9c
Albert Ivanov <albertivanov187@gmail.com>
2025-01-30 15:05:36 +0200

    100/100

--- 04. MatchingBrackets/Program.cs
+++ 04. MatchingBrackets/Program.cs
@@ -1,1 +1,18 @@
 ﻿
+string expression = Console.ReadLine();
+
+Stack<int> stack = new Stack<int>();
+
+for (int i = 0; i < expression.Length; i++)
+{
+	if (expression[i].ToString() == "(")
+	{
+        stack.Push(i);
+    }
+	if (expression[i].ToString() == ")")
+	{
+		int opening = stack.Pop();
+		string substring = expression.Substring(opening, i - opening + 1);
+        Console.WriteLine(substring);
+	}
+}
