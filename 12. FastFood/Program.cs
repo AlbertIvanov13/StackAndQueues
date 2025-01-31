262727a455b7ef510b804b9854ab810eace41e49
Albert Ivanov <albertivanov187@gmail.com>
2025-01-31 15:26:34 +0200

    100/100

---
+++ 12. FastFood/Program.cs
@@ -0,0 +1,28 @@
+﻿
+int foodQuantity = int.Parse(Console.ReadLine());
+
+int[] ordersQuantity = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+Queue<int> queue = new Queue<int>(ordersQuantity);
+
+Console.WriteLine(queue.Max());
+
+while (queue.Count > 0)
+{
+    int nextClient = queue.Peek();
+    if (nextClient <= foodQuantity)
+    {
+        queue.Dequeue();
+        foodQuantity -= nextClient;
+    }
+    else if (nextClient > foodQuantity)
+    {
+        Console.WriteLine($"Orders left: {string.Join(" ", queue)}");
+        break;
+    }
+}
+
+if (queue.Count == 0)
+{
+    Console.WriteLine("Orders complete");
+}
