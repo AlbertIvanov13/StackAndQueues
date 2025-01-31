9406e2132201f828f1cfd9e9316c1c3590b8619e
Albert Ivanov <albertivanov187@gmail.com>
2025-01-31 14:52:32 +0200

    100/100

---
+++ 10. BasicQueueOperations/Program.cs
@@ -0,0 +1,35 @@
+﻿
+int[] operations = Console.ReadLine().Split().Select(int.Parse).ToArray();
+int elementsToEnqueue = operations[0];
+int elementsToDequeue = operations[1];
+int elementsToPeek = operations[2];
+
+int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+Queue<int> queue = new Queue<int>();
+
+for (int i = 0; i < elementsToEnqueue; i++)
+{
+    queue.Enqueue(numbers[i]);
+}
+
+for (int i = 0; i < elementsToDequeue; i++)
+{
+    queue.Dequeue();
+}
+
+if (queue.Count == 0)
+{
+    Console.WriteLine(0);
+}
+else
+{
+    if (queue.Contains(elementsToPeek))
+    {
+        Console.WriteLine("true");
+    }
+    else
+    {
+        Console.WriteLine(queue.Min());
+    }
+}
