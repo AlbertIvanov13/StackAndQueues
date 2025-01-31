ca4b6e4c86a8a79be76c0d8e12e39a46f3ee89a6
Albert Ivanov <albertivanov187@gmail.com>
2025-01-31 22:10:55 +0200

    100/100

---
+++ 13. FashionBoutique/Program.cs
@@ -0,0 +1,25 @@
+﻿
+int[] clothesInTheBox = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+int rackCapacity = int.Parse(Console.ReadLine());
+
+Stack<int> clothes =  new Stack<int>(clothesInTheBox);
+
+int sum = 0;
+int racksCount = 1;
+
+while (clothes.Count > 0)
+{
+    sum += clothes.Peek();
+    if (sum > rackCapacity)
+    {
+        sum = 0;
+        racksCount++;
+    }
+    else
+    {
+        clothes.Pop();
+    }
+}
+
+Console.WriteLine(racksCount);
