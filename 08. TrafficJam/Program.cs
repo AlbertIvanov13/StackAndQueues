5b1a7025d8e5035cdc897c848a6770e453aa1c46
Albert Ivanov <albertivanov187@gmail.com>
2025-01-31 14:43:57 +0200

    100/100

---
+++ 08. TrafficJam/Program.cs
@@ -0,0 +1,42 @@
+﻿
+int passingCars = int.Parse(Console.ReadLine());
+
+string input = Console.ReadLine();
+
+Queue<string> cars = new Queue<string>();
+
+int count = 0;
+while (input != "end")
+{
+	if (input != "green")
+	{
+		cars.Enqueue(input);
+	}
+	else
+	{
+		if (cars.Count == 0)
+		{
+			break;
+		}
+		else
+		{
+			for (int i = 0; i < passingCars; i++)
+			{
+				if (cars.Count != 0)
+				{
+					string passedCar = cars.Dequeue();
+					Console.WriteLine($"{passedCar} passed!");
+					count++;
+				}
+				else
+				{
+					break;
+				}
+			}
+		}
+	}
+
+    input = Console.ReadLine();
+}
+
+Console.WriteLine($"{count} cars passed the crossroads.");
