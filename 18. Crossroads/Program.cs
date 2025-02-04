daf4f9e7c4cd2362989efda4692ecec6553b434b
Albert Ivanov <albertivanov187@gmail.com>
2025-02-04 23:25:01 +0200

    Update

---
+++ 18. Crossroads/Program.cs
@@ -0,0 +1,57 @@
+﻿
+int greenLightDuration = int.Parse(Console.ReadLine());
+int freeWindowDuration = int.Parse(Console.ReadLine());
+
+Queue<string> cars = new Queue<string>();
+
+int count = 0;
+int timeLeft = 0;
+int adding = 0;
+string passingCar = "";
+
+string input = Console.ReadLine();
+
+while (input != "END")
+{
+    if (input == "green")
+    {
+        while (cars.Count > 0)
+        {
+            passingCar = cars.Dequeue();
+            Queue<string> queue = new Queue<string>();
+            Queue<string> newQueue = new Queue<string>();
+            foreach (var item in passingCar)
+            {
+                queue.Enqueue(item.ToString());
+            }
+            if (timeLeft > 0)
+            {
+                for (int i = 0; i < timeLeft; i++)
+                {
+                    if (passingCar.Length > i)
+                    {
+                        queue.Dequeue();
+                    }
+                }
+                while (queue.Count > 0)
+                {
+                    newQueue.Enqueue(queue.Dequeue());
+                }
+                if (queue.Count == 0)
+                {
+                    count++;
+                }
+                timeLeft -= passingCar.Length;
+            }
+        }
+    }
+    else
+    {
+        cars.Enqueue(input);
+        timeLeft = greenLightDuration;
+    }
+    input = Console.ReadLine();
+}
+
+Console.WriteLine("Everyone is safe.");
+Console.WriteLine($"{count} total cars passed the crossroads.");
